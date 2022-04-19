c5eb078099f8308de9df26437c187b9e7c47a6be
Bella-Gomez <93952128+Bella-Gomez@users.noreply.github.com>
2022-04-19 14:48:43 -0700

    Update main.js

--- main.js
+++ main.js
@@ -20,34 +20,44 @@ function Ball(x, y, velX, velY, color, size) {
   this.velY = velY;
   this.color = color;
   this.size = size;
+
 }
 
 Ball.prototype.draw = function() {
   ctx.beginPath();
+
   ctx.fillStyle = this.color;
+  
   ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+
   ctx.fill();
+
 }
 
 Ball.prototype.update = function() {
   if ((this.x + this.size) >= width) {
     this.velX = -(this.velX);
+
   }
 
   if ((this.x - this.size) <= 0) {
     this.velX = -(this.velX);
+
   }
 
   if ((this.y + this.size) >= height) {
     this.velY = -(this.velY);
+
   }
 
   if ((this.y - this.size) <= 0) {
     this.velY = -(this.velY);
+
   }
 
   this.x += this.velX;
   this.y += this.velY;
+
 }
 
 let balls = [];
@@ -58,11 +68,16 @@ while (balls.length < 25) {
     // ball position always drawn at least one ball width
     // away from the edge of the canvas, to avoid drawing errors
     random(0 + size,width - size),
+
     random(0 + size,height - size),
+
     random(-7,7),
+
     random(-7,7),
+
     'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
     size
+
   );
 
   balls.push(ball);
@@ -70,15 +85,18 @@ while (balls.length < 25) {
 
 function loop() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+
   ctx.fillRect(0, 0, width, height);
 
   for (let i = 0; i < balls.length; i++) {
     balls[i].draw();
+
     balls[i].update();
+
   }
 
   requestAnimationFrame(loop);
+
 }
 
 loop();
-
